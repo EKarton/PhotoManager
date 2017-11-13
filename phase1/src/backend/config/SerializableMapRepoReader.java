65aa1e52001d44529a05097cb09a79581f8fc451
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-13 12:59:21 -0500

    created the SerializableMapRepoReader class which is used to read in a serialized map repo from a file

---
+++ phase1/src/backend/config/SerializableMapRepoReader.java
@@ -0,0 +1,11 @@
+package backend.config;
+
+import backend.MapRepository;
+
+public class SerializableMapRepoReader implements ConfigReader {
+
+    @Override
+    public MapRepository getMappings() {
+        return null;
+    }
+}

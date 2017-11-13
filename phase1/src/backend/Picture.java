fcf49d90d6a0d309db364ee18e3c456a85f59131
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-12 23:05:14 -0500

    Added code implementation to PictureManager.java, with implementations to its constructor as well as its update().

--- phase1/src/backend/Picture.java
+++ phase1/src/backend/Picture.java
@@ -4,19 +4,33 @@ import java.util.Observable;
 
 public class Picture extends Observable{
     private String filePath;
+    private String name;
 
-    public Picture(String filePath){
+    public Picture(String filePath, String name){
         this.filePath = filePath;
+        this.name = name;
     }
 
     public String getFilePath(){
         return filePath;
     }
 
+    public String getName(){
+        return name;
+    }
+
     public void setFilePath(String filePath){
+        String oldFilePath = this.filePath;
         this.filePath = filePath;
         this.setChanged();
-        this.notifyObservers(filePath);
+        this.notifyObservers(new Picture(oldFilePath, name));
+    }
+
+    public void setImageName(String name){
+        String oldImageName = this.name;
+        this.name = name;
+        this.setChanged();
+        this.notifyObservers(new Picture(filePath, oldImageName));
     }
 
     @Override

4cea346fb052aba8437dfacf493b7e2343f56d13
trazd66 <tdengzy3@gmail.com>
2017-11-14 14:40:22 -0500

    Reimplemented ConfigMap using Shimi's filewriter

--- phase1/src/backend/logger/configLogs/ConfigMap.java
+++ phase1/src/backend/logger/configLogs/ConfigMap.java
@@ -4,8 +4,9 @@ import java.io.*;
 import backend.MapRepository;
 import backend.Picture;
 import backend.Tag;
+import backend.files.SerializableFileWriter;
 
-public class ConfigMap {
+public class ConfigMap extends SerializableFileWriter {
   private MapRepository<Picture, Tag> mapRepository;
 
   public ConfigMap() {}
@@ -13,19 +14,14 @@ public class ConfigMap {
   public void setMapRepository(MapRepository<Picture, Tag> mapRepository) {
     this.mapRepository = mapRepository;
   }
-  
+
   /**
-   *  write the mapRepo as our configuration file so we can use it to initialize the program when opened.
+   * write the mapRepo as our configuration file so we can use it to initialize the program when
+   * opened.
+   * 
+   * @throws IOException
    */
-  public void writeConig() {
-    try {
-      FileOutputStream config = new FileOutputStream("/config.log");
-      ObjectOutputStream configFile = new ObjectOutputStream(config);
-      configFile.writeObject(this.mapRepository);
-      configFile.close();
-      config.close();
-    } catch (IOException i) {
-      i.printStackTrace();
-    }
+  public void writeConfig() throws IOException {
+    super.write("/config.log", false, this.mapRepository);
   }
 }

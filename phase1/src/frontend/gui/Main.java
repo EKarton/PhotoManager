6f29a2cb432ede487796a2e24dbbc462a1de7a28
shimismith <smith.shimi@gmail.com>
2017-11-13 23:20:06 -0500

    I fixed an interesting bug where I was getting null pointers when using the menu bar. I learned that javaFX creates it's own instance of MainView, in which actionEventController was null. I fixed this by adding a constructor that initializes actionEventController.

--- phase1/src/frontend/gui/Main.java
+++ phase1/src/frontend/gui/Main.java
@@ -2,7 +2,6 @@ package frontend.gui;
 
 public class Main {
   public static void main(String[] args){
-    MainView mainView = new MainView();
-    mainView.startMainView(args);
+    MainView.startMainView(args);
   }
 }

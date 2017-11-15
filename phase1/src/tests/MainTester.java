6c0881ea94f8078de9a2f2c87c6912a5ebb2b050
shimismith <smith.shimi@gmail.com>
2017-11-14 19:26:15 -0500

    I had to resolve a conflict with MainTester as someone commited changes to it. From now we need to do all testing each on our own testing branchs.

--- phase1/src/tests/MainTester.java
+++ phase1/src/tests/MainTester.java
@@ -1,23 +1,7 @@
 package tests;
 
-import backend.BiDirectionalHashMap;
-import backend.Picture;
-import backend.Tag;
-
 public class MainTester {
-    public static void main(String[] args){
-        BiDirectionalHashMap<Picture, Tag> repo = new BiDirectionalHashMap<>();
-        Picture picture = new Picture("C:\\Users\\wasd.png");
-        Tag tag1 = new Tag("Dog");
-        Tag tag2 = new Tag("Cat");
-        repo.addKeyWithValue(picture, tag1);
-        repo.addValueToKey(picture, tag2);
+    public static void main(String[] args) {
 
-        Picture picture2 = new Picture("");
-
-        System.out.println(repo);
-
-        repo.deleteKey(picture);
-        System.out.println(repo);
     }
 }

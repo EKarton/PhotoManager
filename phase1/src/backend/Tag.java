b179d108eceb716b20a692ce9bdac8cf384c8342
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-15 22:43:30 -0500

    Removed description from tag class

--- phase1/src/backend/Tag.java
+++ phase1/src/backend/Tag.java
@@ -5,17 +5,10 @@ import java.util.Observable;
 
 public class Tag extends Observable implements Serializable{
     private String label;
-    private String description;
 
-    public Tag (String label, String description){
-        this.label = label;
-        this.description = description;
-    }
-    
     public Tag (String label){
-      this.label = label;
-      this.description = "no description given";
-  }
+        this.label = label;
+    }
 
     public String getLabel(){
         return label;
@@ -26,13 +19,6 @@ public class Tag extends Observable implements Serializable{
         this.label = label;
         this.setChanged();
         this.notifyObservers(oldTag);
-    }
-    
-    public void setDescription(String description){
-      String oldDescription = this.description;
-      this.description = description;
-      this.setChanged();
-      this.notifyObservers(oldDescription);
     }
 
     @Override

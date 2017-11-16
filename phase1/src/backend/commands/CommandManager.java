214bf7da356cf3af8d6e74c0e0913dab8bb369ad
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-16 01:21:22 -0500

    Added implementation to CommandManager class

--- phase1/src/backend/commands/CommandManager.java
+++ phase1/src/backend/commands/CommandManager.java
@@ -6,8 +6,20 @@ import javax.naming.NoInitialContextException;
 public class CommandManager {
   private LinkedList<Command> commandStack = new LinkedList<>();
 
+  private boolean areCommandsLimited = false;
+  private int maxNumCommands = 10;
+
+  public CommandManager(){
+  }
+
+  public CommandManager(boolean limitStoredCommands, int maxNumCommands){
+    this.areCommandsLimited = limitStoredCommands;
+    this.maxNumCommands = maxNumCommands;
+  }
+
   public void addCommand(Command command){
-    this.commandStack.push(command);
+    if (areCommandsLimited && maxNumCommands > commandStack.size())
+      this.commandStack.push(command);
   }
 
   public void undoRecentCommand() throws NoInitialContextException {

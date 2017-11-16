b1c8f7cdd417dd1b1c2b50f38db4f74a9e520737
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-16 01:14:37 -0500

    Added implementatin to the DeleteTagFromPictureCommand class

--- phase1/src/backend/commands/DeleteTagFromPictureCommand.java
+++ phase1/src/backend/commands/DeleteTagFromPictureCommand.java
@@ -1,38 +1,63 @@
 package backend.commands;
 
+import backend.Picture;
+import backend.PictureManager;
+import backend.Tag;
+
+/**
+ * A command used to delete a tag from a picture.
+ */
 public class DeleteTagFromPictureCommand implements Command {
 
   /**
-   * Redos the command again
+   * A picture containing the tag to delete
    */
-  @Override
-  public void redo() {
+  private Picture picture;
 
+  /**
+   * The tag to delete.
+   */
+  private Tag tag;
+
+  /**
+   * Creates a DeleteTagFromPictureCommand specifying a tag to delete from a picture.
+   * Note: the tag should be already in the picture.
+   * @param picture A picture containing the tag
+   * @param tag The tag to delete.
+   */
+  public DeleteTagFromPictureCommand(Picture picture, Tag tag){
+    this.picture = picture;
+    this.tag = tag;
   }
 
   /**
-   * Undo the command
+   * Undo the command by re-adding the tag back to the picture.
+   * If for some reason the tag already exist in the picture,
+   * it will not add the tag again to the picture.
    */
   @Override
   public void undo() {
-
+    if (!picture.containsTag(tag))
+      picture.addTags(tag);
   }
 
   /**
-   * Execute the command
+   * Execute the command by removing the tag from the picture.
+   * If for some reason the tag is already not in the picture,
+   * it will do nothing.
    */
   @Override
   public void execute() {
-
+    if (picture.containsTag(tag))
+      picture.deleteTag(tag);
   }
 
   /**
    * Returns a log message for this command
-   *
    * @return A log message for this command
    */
   @Override
   public String getLogMessage() {
-    return null;
+    return "Removed a tag " + tag + " from picture " + picture;
   }
 }

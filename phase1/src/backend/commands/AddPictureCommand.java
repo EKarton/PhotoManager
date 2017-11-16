8f2e09d519d49c4d68ba15f49a07a18387a47f8a
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-16 00:57:24 -0500

    Added a command called AddPictureCommand where it will execute nessessary steps to add a picture to the PictureManager. It can also undo this action

--- phase1/src/backend/commands/AddPictureCommand.java
+++ phase1/src/backend/commands/AddPictureCommand.java
@@ -3,46 +3,56 @@ package backend.commands;
 import backend.Picture;
 import backend.PictureManager;
 
+/**
+ * A class used to specify an addition of a picture to a manager.
+ */
 public class AddPictureCommand implements Command {
 
+  /**
+   * A picture to add
+   */
   private Picture picture;
+
+  /**
+   * The manager to hold the pictures.
+   */
   private PictureManager manager;
 
+  /**
+   * Creates an AddPictureCommand instance
+   * @param picture A picture to add
+   * @param manager A manager used to store the pictures.
+   */
   public AddPictureCommand(Picture picture, PictureManager manager){
     this.picture = picture;
     this.manager = manager;
   }
 
   /**
-   * Redos the command again
-   */
-  @Override
-  public void redo() {
-
-  }
-
-  /**
-   * Undo the command
+   * Undo the command. It will remove the picture from the manager.
    */
   @Override
   public void undo() {
+    if (manager.contains(picture))
+      manager.untrackPicture(picture);
   }
 
   /**
-   * Execute the command
+   * Execute the command by adding a picture to the picture manager.
+   * It will only add it if the picture does not exist in the manager yet.
    */
   @Override
   public void execute() {
-    manager.addPicture(picture);
+    if (!manager.contains(picture))
+      manager.addPicture(picture);
   }
 
   /**
    * Returns a log message for this command
-   *
    * @return A log message for this command
    */
   @Override
   public String getLogMessage() {
-    return null;
+    return "Added picture " + picture + " to the Picture Manager, " + manager;
   }
 }

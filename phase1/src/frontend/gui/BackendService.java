17483d9b9453e72187439cb2e353654c7a732a8b
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2017-11-18 22:42:24 -0500

    Added an update to the BackendService where it will load / save the app config

--- phase1/src/frontend/gui/BackendService.java
+++ phase1/src/frontend/gui/BackendService.java
@@ -6,47 +6,82 @@ import backend.models.AppSettings;
 import backend.models.PictureManager;
 import javafx.application.Platform;
 
+/**
+ * A service class used to simplify the calls to the backend services.
+ */
 public class BackendService {
   private CommandManager commandManager;
   private PictureManager pictureManager;
   private AppSettings appSettings;
 
+  /**
+   * Constructs a new BackendService.
+   * When the app settings was not found /corrupted /
+   * locked by another application,
+   * it will create a new app settings file.
+   */
   public BackendService() {
     try {
-      commandManager = new CommandManager();
-      pictureManager = new PictureManager();
-      appSettings = new AppSettings();
-    } catch (IOException e) {
-      // TODO add fail safe
-      Platform.exit();
+      this.commandManager = new CommandManager();
+      this.pictureManager = new PictureManager();
+      this.appSettings = AppSettings.loadFromFile();
+      this.appSettings.addPicToManager(pictureManager);
+    }
+    catch (IOException | ClassNotFoundException e) {
+      this.appSettings = new AppSettings();
     }
   }
 
+  /**
+   * Resets the backend service by supplying the PictureManager
+   * with a new set of pictures in a specific location in the OS.
+   * @param directory The directory to grab pictures from
+   * @param isRecursive True if to grab pictures recursively; else false.
+   */
   public void resetBackendService(String directory, boolean isRecursive) {
     try {
-      pictureManager = new PictureManager(directory, isRecursive);
-      appSettings.addPicToManager(pictureManager);
-    } catch (IOException e) {
-      //TODO add fail safe
+      this.pictureManager = new PictureManager(directory, isRecursive);
+      this.appSettings.addPicToManager(pictureManager);
+    }
+    catch (IOException e) {
     }
   }
 
+  /**
+   * Returns the current command manager.
+   * @return The current command manager.
+   */
   public CommandManager getCommandManager() {
     return this.commandManager;
   }
 
+  /**
+   * Returns the current picture manager
+   * @return The current picture manager.
+   */
   public PictureManager getPictureManager() {
     return this.pictureManager;
   }
-  
+
+  /**
+   * Returns the current app settings for this app.
+   * @return The current app settings
+   */
   public AppSettings getAppSettings() {
     return this.appSettings;
   }
-  
+
+  /**
+   * Saves the app settings object called Config.
+   * It will also update the app settings by appending
+   * it with the list of pictures from the PictureManager
+   */
   public void save() {
     try {
+      this.appSettings.addPicFromManager(this.pictureManager);
       this.appSettings.save();
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       // This should never happen, but if it does just don't save
     }
   }

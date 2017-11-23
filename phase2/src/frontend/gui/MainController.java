335c6d4e74baef9c376c2830d770588a99cf2a77
shimismith <smith.shimi@gmail.com>
2017-11-23 13:26:07 -0500

    Added on action for menu bar

--- phase2/src/frontend/gui/MainController.java
+++ phase2/src/frontend/gui/MainController.java
@@ -4,11 +4,39 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-public class MainController implements Initializable{
+public class MainController implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     System.out.println("GUI has just started");
+  }
+  
+  public void openDirectory() {
+    // TODO fill in
+  }
+  
+  public void openDirectryRecursively() {
+    // TODO fill in
+  }
+  
+  public void openLog() {
+    // TODO fill in
+  }
+  
+  public void manageTags() {
+    // TODO fill in
+  }
+  
+  public void save() {
+    // TODO fill in
+  }
+  
+  public void undo() {
+    // TODO fill in
+  }
+  
+  public void redo() {
+    // TODO fill in
   }
 
 }
